2352e0ddfcfc74111f09c0fc5e15f6551c7e704c
s-threeroots <shinichiro.mine@threeroots.tech>
2021-06-27 13:18:48 +0900

    とりあえず見るようのテストデータ

---
+++ testdata.sql
@@ -0,0 +1,84 @@
+insert into estimations
+(
+	created_at,
+	updated_at,
+	deleted_at,
+	client_name,
+	estimation_name,
+	sub_total,
+	tax,
+	total
+)
+values (
+	current_timestamp,
+	current_timestamp,
+	null,
+	'client',
+	'estimation',
+	3000,
+	300,
+	3300
+)
+
+select * from estimations
+
+
+insert into groups 
+(
+	created_at,
+	updated_at,
+	deleted_at,
+	estimation_id,
+	name
+)
+values (
+	current_timestamp,
+	current_timestamp,
+	null,
+	1,
+	'group'
+)
+
+insert into items
+(
+	created_at,
+	updated_at,
+	deleted_at,
+	group_id,
+	name,
+	amount,
+	unit,
+	unit_price
+)
+values (
+	current_timestamp,
+	current_timestamp,
+	null,
+	1,
+	'name1',
+	10,
+	'unit1',
+	200
+)
+
+insert into items
+(
+	created_at,
+	updated_at,
+	deleted_at,
+	group_id,
+	name,
+	amount,
+	unit,
+	unit_price
+)
+values (
+	current_timestamp,
+	current_timestamp,
+	null,
+	1,
+	'name2',
+	10,
+	'unit2',
+	100
+)
